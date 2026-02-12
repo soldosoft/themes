d90d5599edbef85e49e99a993f94169551fb0cbb
SoldoSoft3 <soldosoft@gmail.com>
2026-02-12 19:49:01 +0000

    feat: Add Filament v4 compatibility
    - Update composer.json to require filament/filament ^4.0
    - Fix $navigationIcon type to BackedEnum|string|null (Filament v4 widened type)
    - Fix $view property from static to non-static (Filament v4 SimplePage change)

--- src/Filament/Pages/Themes.php
+++ src/Filament/Pages/Themes.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Arr;
 
 class Themes extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static \BackedEnum|string|null $navigationIcon = 'heroicon-o-document-text';
 
     protected static ?string $title = 'Appearance';
 
@@ -22,7 +22,7 @@ class Themes extends Page
         return __('themes::themes.appearance');
     }
 
-    protected static string $view = 'themes::filament.pages.themes';
+    protected string $view = 'themes::filament.pages.themes';
 
     public function mount(): void
     {
